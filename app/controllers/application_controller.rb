559cb4a3cfc6c2290afdcf2fccf14064f65c478a
zachary mines <zmines@wesleyan.edu>
2020-09-17 21:19:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -48,7 +48,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-    @user = User.find_by_id(session[:user_id])
+    @user = User.find(session[:user_id])
     erb :'/users/home'
   end
 end
