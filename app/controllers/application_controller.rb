4822f8aba39f8da9293a592c3aaa5cedd289cb30
zachary mines <zmines@wesleyan.edu>
2020-09-17 21:04:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,6 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    binding.pry
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
